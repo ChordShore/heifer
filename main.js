453782d7ec451e5d1715e4cb31bc7fc0d0797afb
ChordShore <contact@chordshore.com>
2026-01-05 19:42:06 -0800

    Added cowsay functionality.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import { moo } from "./moo.js";
+import * as cowsay from "cowsay";
+
 let name = "Friend";
 
 function main(name) {
     //console.log(`moo, ${name}`)
-    console.log(moo(name));
+    //console.log(moo(name));
+    console.log(cowsay.say({
+        text : moo(name)
+    }));
 }
 
 main(name);
